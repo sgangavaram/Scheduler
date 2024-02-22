34856966928362135e9306bfa27301d6fbde5301
PENDYALA HARSHA VARDHAN <harshasunny98@gmail.com>
2024-02-22 11:07:08 +0530

    fixed getTimeRequired issue

--- assigning.py
+++ assigning.py
@@ -11,6 +11,23 @@ TIME_REQUIRED_FOR_BASEMENT_CHANGE = 90  # meter per sec.
 skipped_jobs = []
 tenant_data = None
 
+def getTimeRequired(t1, t2):
+    # returns time required in seconds
+    global tenant_data
+    # print(tenant_data)
+    # Distance = distance((location1['latitude'],location1['longitude']), (location2['latitude'],location2['longitude'])).m
+    Distance = distance((tenant_data['tenantBlocks'][t1]['locationCoordinates']['latitude'], tenant_data['tenantBlocks'][t1]['locationCoordinates']['longitude']),(tenant_data['tenantBlocks'][t2]['locationCoordinates']['latitude'], tenant_data['tenantBlocks'][t2]['locationCoordinates']['longitude'])).m
+    return Distance / VELOCITY
+
+
+def timeRequired(jobA, jobB):
+    # indA = tower_index[jobA.tower]
+    # indB = tower_index[jobB.tower]
+    # time = travellingTimes[indA][indB]
+    time = getTimeRequired(jobA.tenantBlockId, jobB.tenantBlockId)
+    time += TIME_REQUIRED_FOR_BASEMENT_CHANGE * abs(int(jobA.basement[1]) - int(jobB.basement[1]))
+    return time / 3600
+
 def assignJobs(tenant, jobs, workers):
     global tenant_data
     tenant_data = tenant
